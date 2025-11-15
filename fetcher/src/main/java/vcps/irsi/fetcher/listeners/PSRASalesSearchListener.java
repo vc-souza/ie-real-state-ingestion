1229964e3800666e6afc24146cb1036936d66a8f
vc-souza <vinicius.cp.souza@gmail.com>
2025-11-15 16:30:07 +0000

    Handle search requests from Kafka

--- fetcher/src/main/java/vcps/irsi/fetcher/listeners/PSRASalesSearchListener.java
+++ fetcher/src/main/java/vcps/irsi/fetcher/listeners/PSRASalesSearchListener.java
@@ -1,5 +1,7 @@
 package vcps.irsi.fetcher.listeners;
 
+import java.time.Duration;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -7,9 +9,9 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
-import vcps.irsi.fetcher.config.PSRASupplierConfig;
 import vcps.irsi.fetcher.dto.messages.PSRASalesSearchRequest;
 import vcps.irsi.fetcher.services.fetching.IFetcher;
+import vcps.irsi.fetcher.services.throttling.ThrottledRequestException;
 
 /**
  * TODO: doc
@@ -18,18 +20,34 @@ import vcps.irsi.fetcher.services.fetching.IFetcher;
 @Service
 public class PSRASalesSearchListener {
     private final IFetcher<PSRASalesSearchRequest> fetcher;
-    public final PSRASupplierConfig supplierConfig;
 
-    public PSRASalesSearchListener(IFetcher<PSRASalesSearchRequest> fetcher, PSRASupplierConfig supplierConfig) {
+    public PSRASalesSearchListener(IFetcher<PSRASalesSearchRequest> fetcher) {
         this.fetcher = fetcher;
-        this.supplierConfig = supplierConfig;
     }
 
-    @KafkaListener(topics = "#{ __listener.supplierConfig.getSales().getSearchTopic() }")
-    public void listen(@Payload PSRASalesSearchRequest request, ConsumerRecord<?, ?> record, Acknowledgment ack) {
-        log.info("Processing message {} (p={},o={},t={}) <headers={}>", request, record.partition(),
-                record.offset(), record.timestamp(), record.headers().toString());
+    @KafkaListener(topics = "${suppliers.psra.sales.search-topic}")
+    public void listen(
+            @Payload PSRASalesSearchRequest request,
+            ConsumerRecord<?, ?> record,
+            Acknowledgment ack) {
 
-        // TODO: impl
+        log.info("Processing message {} (p={},o={},t={})", request, record.partition(), record.offset(),
+                record.timestamp());
+
+        try {
+            boolean fetched = fetcher.fetch(request);
+
+            log.info(fetched ? "Request processed" : "Skipping: request already processed");
+
+            ack.acknowledge();
+
+        } catch (ThrottledRequestException e) {
+            log.error("Giving up for now, operation was throttled", e);
+            ack.nack(Duration.ofHours(1));
+
+        } catch (InterruptedException e) {
+            log.error("Message handling interrupted", e);
+            Thread.currentThread().interrupt();
+        }
     }
 }
